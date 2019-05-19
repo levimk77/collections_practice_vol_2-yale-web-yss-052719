0f5d3d23fffe204cfb8db4f8fbb99b5a5af1b63a
Elijah Florence <efloren@siue.edu>
2019-05-19 22:43:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,22 @@ def begins_with_r(array)
   testarray = []
   array.each do |value|
     if value.first == "r"
-      
-    else
-      return FALSE
+      else
+    testarray << " "
+  end
+  if testarray.length > 0 
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def contain_a(array)
+  array.each do |value|
+    value.each do |letter|
+      if letter == "a"
+        return value
+      else
+    end
+  end
 end
